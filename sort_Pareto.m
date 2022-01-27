1c3dea4c3f447c935e9a31e9700cc90cffe745ae
BachDo17 <55902248+BachDo17@users.noreply.github.com>
2022-01-27 22:42:50 +0900

    Delete sort_Pareto.m

--- sort_Pareto.m
+++
@@ -1,9 +0,0 @@
-function [Pareto_x,Pareto_y,Dominated_y]=sort_Pareto(candidates,obj)
- Pareto = paretoFront(obj);
- Pareto_y = obj(Pareto,:);
- Dominated_y = setdiff(obj,Pareto_y,'rows');
- %Dominated_y = unique(Dominated_y,'rows','stable');
- ID_sol = sortID2(obj,Pareto_y);
- Pareto_x = candidates(ID_sol,:);
- %Pareto_x = unique(solution,'rows','stable');
-end
